ba37ce0d597ab5e560308deaaaf9b7a7eaf96033
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-10-28 14:54:54 +0100

    new commit

--- script.js
+++ script.js
@@ -13,54 +13,54 @@ delay(1500).then(logger) // Resolved after 1500ms
 
 //? Task 2
 
-// const users = [
-//   { name: "Mango", active: true },
-//   { name: "Poly", active: false },
-//   { name: "Ajax", active: true },
-//   { name: "Lux", active: false },
-// ]
-// const toggleUserState = (allUsers, userName) => {
-//   return new Promise((res, rej) => {
-//     const updatedUsers = allUsers.map((user) =>
-//       user.name === userName ? { ...user, active: !user.active } : user
-//     )
-//     res(updatedUsers)
-//   })
-// }
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+]
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((res, rej) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    )
+    res(updatedUsers)
+  })
+}
 
-// const logger = (updatedUsers) => console.table(updatedUsers)
-// toggleUserState(users, "Mango").then(logger)
-// toggleUserState(users, "Lux").then(logger)
+const logger1 = (updatedUsers) => console.table(updatedUsers)
+toggleUserState(users, "Mango").then(logger1)
+toggleUserState(users, "Lux").then(logger1)
 
 // //? Task 3
 
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min)
-// }
-// const makeTransaction = (transaction) => {
-//   return new Promise((res, rej) => {
-//     const delay = randomIntegerFromInterval(200, 500)
-//     setTimeout(() => {
-//       const canProcess = Math.random() > 0.3
-//       if (canProcess) {
-//         res({ id: transaction.id, time: delay })
-//       } else {
-//         rej(transaction.id)
-//       }
-//     }, delay)
-//   })
-// }
-// const logSuccess = ({ id, time }) => {
-//   console.log(`Transaction ${id} processed in ${time} ms`)
-// }
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+const makeTransaction = (transaction) => {
+  return new Promise((res, rej) => {
+    const delay = randomIntegerFromInterval(200, 500)
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3
+      if (canProcess) {
+        res({ id: transaction.id, time: delay })
+      } else {
+        rej(transaction.id)
+      }
+    }, delay)
+  })
+}
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`)
+}
 
-// const logError = (id) => {
-//   console.warn(`Error processing transaction ${id}. Please try again later.`)
-// }
-// makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError)
+const logError = (id) => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`)
+}
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError)
 
-// makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError)
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError)
 
-// makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError)
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError)
 
-// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError)
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError)
